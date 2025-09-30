6d256f2ddf8ecb142359d17ddcf27c1751994a7b
dntpngme <tmurka1337@gmail.com>
2025-09-30 16:19:16 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const isAuth = request.cookies.get('next-auth.session-token') || request.cookies.get('next-auth.session');
   const isWelcome = request.nextUrl.pathname === '/welcome';
-  if (!isAuth && !isWelcome && request.nextUrl.pathname !== '/api/auth') {
+  if (!isAuth && !isWelcome && request.nextUrl.pathname !== '/auth/register') {
     return NextResponse.redirect(new URL('/welcome', request.url));
   }
   return NextResponse.next();
@@ -13,3 +13,4 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: ['/((?!_next|api|favicon.ico|public|styles|generated|prisma|dotnet-frontend).*)'],
 };
+
