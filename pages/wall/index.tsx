cacdc3595a5d4fc1a7d0511c837ea0da7d40121f
Тимур <timurnazaev001.com>
2025-10-02 23:03:37 +0700

    Update Linker - Stable patch

---
+++ pages/wall/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WallPage: React.FC = () => {
+	return (
+		<div style={{color:'#fff',textAlign:'center',marginTop:40,fontSize:28}}>
+			Стена
+		</div>
+	);
+};
+
+export default WallPage;
