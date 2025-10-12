bda39a0301e7165170687a60efd4ad99d136bc4c
Тимур <timurnazaev001.com>
2025-10-12 17:10:22 +0700

    Update v4.1

--- pages/welcome.tsx
+++
@@ -1,7 +0,0 @@
-import { FaLock, FaUserSecret, FaBolt } from "react-icons/fa";
-import { motion } from "framer-motion";
-import Link from "next/link";
-
-export default function Welcome() {
-// Страница удалена, редирект теперь на /auth/register
-}
