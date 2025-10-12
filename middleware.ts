7b4a9491ba5c03b1aa456562385d6b5d44198f8b
Тимур <timurnazaev001.com>
2025-10-12 17:11:51 +0700

    Update v4.1

--- middleware.ts
+++ middleware.ts
@@ -5,13 +5,9 @@ export function middleware(request: import('next/server').NextRequest) {
     const isAuth = request.cookies.get('next-auth.session-token') || request.cookies.get('next-auth.session');
     const path = request.nextUrl.pathname;
 
-    // При заходе на корень "/" редиректим на /welcome
+    // При заходе на корень "/" редиректим на /auth/register
     if (path === '/') {
-      return NextResponse.redirect(new URL('/welcome', request.url));
-    }
-
-    if (isAuth && path === '/welcome') {
-      return NextResponse.redirect(new URL('/profile', request.url));
+      return NextResponse.redirect(new URL('/auth/register', request.url));
     }
 
     return NextResponse.next();
