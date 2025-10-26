6d3b50f6227079afa41e4754e6d33355786b2725
Тимур <timurnazaev001.com>
2025-10-26 17:38:26 +0700

    Update

--- pages/api/media/db/[id]/audio.ts
+++ pages/api/media/db/[id]/audio.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '../../../auth/[...nextauth]';
-import prisma from '../../../../lib/prisma';
+import prisma from '../../../../../lib/prisma';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' });
