62c6a2c686a360768c0ec6acb5b2e542eda8a2b8
Тимур <timurnazaev001.com>
2025-11-07 21:34:03 +0700

    Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -335,6 +335,10 @@ function MainApp({ Component, pageProps }: AppProps) {
         {/* Mobile meta tags for responsive layout and theme color */}
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover" />
         <meta name="theme-color" content="#0f1214" />
+        {/* Load Poppins early (preconnect + stylesheet) to ensure font is available */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         {/* reCAPTCHA script moved to registration page to avoid loading globally */}
       </Head>
       {showSidebar && <Sidebar />}
