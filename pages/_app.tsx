2ce6e370584338bb4d9d03aef5c446a1bf5d90de
Тимур <timurnazaev001.com>
2025-11-08 17:33:31 +0700

    Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -359,6 +359,13 @@ function MainApp({ Component, pageProps }: AppProps) {
               <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
               <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@200;300;400;500;600;700&display=swap" rel="stylesheet" />
             </>
+
+            {/* Load Roboto as the default UI font */}
+            <>
+              <link rel="preconnect" href="https://fonts.googleapis.com" />
+              <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+              <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" />
+            </>
         {/* reCAPTCHA script moved to registration page to avoid loading globally */}
       </Head>
       {showSidebar && <Sidebar />}
